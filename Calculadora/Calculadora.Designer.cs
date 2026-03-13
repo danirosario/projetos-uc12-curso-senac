31652290332ad987a6bcf4c8009cb488990a4a0d
daniela.rosario <daniela.trosario@gmail.com>
2026-03-13 11:54:05 -0300

    Desabilita maximização da janela da calculadora

--- Calculadora/Calculadora.Designer.cs
+++ Calculadora/Calculadora.Designer.cs
@@ -29,22 +29,36 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculadora));
+            this.painel = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // painel
+            // 
+            this.painel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.painel.Location = new System.Drawing.Point(12, 12);
+            this.painel.Name = "painel";
+            this.painel.Size = new System.Drawing.Size(256, 62);
+            this.painel.TabIndex = 0;
             // 
             // Calculadora
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(280, 455);
+            this.Controls.Add(this.painel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Calculadora";
             this.Text = "Calculadora";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox painel;
     }
 }
 
