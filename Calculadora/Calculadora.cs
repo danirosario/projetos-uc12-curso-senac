9fae52fedab9a9a377c4e337f2210134ccab358b
danirosario <rosario.dani01@gmail.com>
2026-03-17 10:10:27 -0300

    add lógica nos botões numéricos para que apareça no painel; add lógica no 0 para que não se repita se não houver outros numeros;

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,56 +20,132 @@ namespace Calculadora
 
         private void botaoNumero0_Click(object sender, EventArgs e)
         {
-            textBoxPainel.Text += "0"; // quando clicar no botão 0, o painel de texto exibirá "0"
+            if(textBoxPainel.Text != "0")
+    {
+                textBoxPainel.Text += "0";
+            }
+
         }
         private void botaoNumero1_Click(object sender, EventArgs e)
         {
-            textBoxPainel.Text += "1"; 
+            if (textBoxPainel.Text == "0")
+            {
+                textBoxPainel.Text = "1";
+            }
+            else
+            {
+                textBoxPainel.Text += "1";
+            }
         }
 
         private void botaoNumero2_Click(object sender, EventArgs e)
         {
-            textBoxPainel.Text += "2";
+            if (textBoxPainel.Text == "0")
+            {
+                textBoxPainel.Text = "2";
+            }
+            else
+            {
+                textBoxPainel.Text += "2";
+            }
         }
 
         private void botaoNumero3_Click(object sender, EventArgs e)
         {
-            textBoxPainel.Text += "3";
+            if (textBoxPainel.Text == "0")
+            {
+                textBoxPainel.Text = "3";
+            }
+            else
+            {
+                textBoxPainel.Text += "3";
+            }
         }
 
         private void botaoNumero4_Click(object sender, EventArgs e)
         {
-            textBoxPainel.Text += "4";
+            if (textBoxPainel.Text == "0")
+            {
+                textBoxPainel.Text = "4";
+            }
+            else
+            {
+                textBoxPainel.Text += "4";
+            }
         }
 
         private void botaoNumero5_Click(object sender, EventArgs e)
         {
-            textBoxPainel.Text += "5";
+            if (textBoxPainel.Text == "0")
+            {
+                textBoxPainel.Text = "5";
+            }
+            else
+            {
+                textBoxPainel.Text += "5";
+            }
         }
 
         private void botaoNumero6_Click(object sender, EventArgs e)
         {
-            textBoxPainel.Text += "6";
+            if (textBoxPainel.Text == "0")
+            {
+                textBoxPainel.Text = "6";
+            }
+            else
+            {
+                textBoxPainel.Text += "6";
+            }
         }
 
         private void botaoNumero7_Click(object sender, EventArgs e)
         {
-            textBoxPainel.Text += "7";
+            if (textBoxPainel.Text == "0")
+            {
+                textBoxPainel.Text = "7";
+            }
+            else
+            {
+                textBoxPainel.Text += "7";
+            }
         }
 
         private void botaoNumero8_Click(object sender, EventArgs e)
         {
-            textBoxPainel.Text += "8";
+            if (textBoxPainel.Text == "0")
+            {
+                textBoxPainel.Text = "8";
+            }
+            else
+            {
+                textBoxPainel.Text += "8";
+            }
         }
 
         private void botaoNumero9_Click(object sender, EventArgs e)
         {
-            textBoxPainel.Text += "9";
+            if (textBoxPainel.Text == "0")
+            {
+                textBoxPainel.Text = "9";
+            }
+            else
+            {
+                textBoxPainel.Text += "9";
+            }
         }
 
         private void botaoVirgula_Click(object sender, EventArgs e)
         {
-            textBoxPainel.Text += ",";
+            if(textBoxPainel.Text == "") // Verifica se o painel de texto está vazio
+            {
+                textBoxPainel.Text = "0,"; // Se estiver vazio, adiciona "0," para iniciar um número decimal
+            }
+            
+            if (!textBoxPainel.Text.Contains(",")) // Verifica se o painel de texto já contém uma vírgula
+            {
+                textBoxPainel.Text += ",";
+            }
+            
         }
     }
 }
