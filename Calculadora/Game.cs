4af63bfea3af43d65bfbf503acf3d5fc1bf09e7b
danirosario <rosario.dani01@gmail.com>
2026-03-19 09:40:09 -0300

    add classe game; add imagens para determinar o resultado do jogo com base na escolha do jogador e do computador;

---
+++ Calculadora/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Game
+    {
+        public enum Resultado
+        {
+            Ganhar,
+            Perder,
+            Empatar
+        }
+
+        public static Image[] imagens =
+        {
+            Image.FromFile("imagens/Pedra.png"),
+            Image.FromFile("imagens/Papel.png"),
+            Image.FromFile("imagens/Tesoura.png")
+        };
+
+        // Método para determinar o resultado do jogo com base na escolha do jogador e do computador
+        public Image ImgPc { get; set; }
+        public Image ImgJogador { get; set; }
+    }
+}
